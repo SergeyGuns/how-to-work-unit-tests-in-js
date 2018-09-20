2e4b9e07cd556d953edbdecd28d07eb226bef3f1
Sergey <mheguevara2@gmail.com>
2018-09-20 19:33:49 +0300

    whith-space

--- home-work/parser.js
+++ home-work/parser.js
@@ -1,7 +1,18 @@
-const objParserToHtml = (data) => {
+const objParserToHtml = (data, space = '') => {
+  // console.log(data)
+  return `${space}<${data.tag}${data.attributes ? 
+    Object.keys(data.attributes).map(attr=> ` ${attr}="${data.attributes[attr]}"`) 
+    :
+    ''}>${data.childs.map((child)=>{
+      if(typeof child === 'string') {
+        return child
+      } else {
+        return '\n' + objParserToHtml(child, data.tag!=='html'?space+'  ':space)
+      }
+    }).join('')}${!~['li','title'].indexOf(data.tag)?'\n'+space:''}</${data.tag}>`
+
   //you code
   
-  return data
 }
 
 module.exports = {
