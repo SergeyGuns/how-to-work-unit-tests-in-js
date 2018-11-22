7a57a5bffefe93349848a0974b96232ca495e316
Sergey <mheguevara2@gmail.com>
2018-11-22 19:13:01 +0300

    carring

--- home-work-carring/index.js
+++ home-work-carring/index.js
@@ -1,20 +1,6 @@
-function carringOne(arg) {
-  let res = 0
-  if(arg) {
-    res+=arg
-    return arg => arg ? carringOne(res+arg) : res
-  }
-  return res
-}
 
-function carringMultiple(...arg) {
-  let res = 0
-  if(arg.length) {
-    res += arg.reduce((acc, curr) => acc + curr, res)
-    return (...arg) => arg.length ? carringMultiple(...arg,res) : res
-  }
-  return res
-}
-console.log(carringOne(1)(2)(3)(4)(5)())
+console.log(carring(2)(3)) // 5
 
-console.log(carringMultiple(1,2,3,4)(1,2,3,4,5)(1,3,4,5)(2)())
+console.log(carringOne(1)(2)(3)(4)(5)()) // summ
+
+console.log(carringMultiple(1,2,3,4)(1,2,3,4,5)(1,3,4,5)(2)()) //summ
